7d8b18fa78fe531d8c13f2bff41254da54f644eb
Noob Hack <273600329+ThT0AltayHR@users.noreply.github.com>
2026-09-12 18:02:41 +0000

    Align Kotlin with Compose compiler

--- sqlmap_app_structure/settings.gradle.kts
+++ sqlmap_app_structure/settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.2.2"
-        kotlin("android") version "1.9.22"
-        kotlin("kapt") version "1.9.22"
+        kotlin("android") version "1.9.23"
+        kotlin("kapt") version "1.9.23"
         id("com.google.dagger.hilt.android") version "2.50"
     }
 }
